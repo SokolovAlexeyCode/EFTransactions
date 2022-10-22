97e5d2fbc1ebb942a4afa243ab278b665d0f5451
Sokolov Aleksei <sokolov.alexey.mail@gmail.com>
2022-10-22 20:51:25 +0400

    Add project files.

---
+++ EFTransactions/EFClasses/Entities/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace EFTransactions.EFClasses.Entities;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public DateTime LastProductAccess { get; set; }
+}
---
+++ EFTransactions/EFClasses/Infrastructure/DemoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EFTransactions.EFClasses.Entities;
+
+namespace EFTransactions.EFClasses.Infrastructure;
+
+public class DemoDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    public DemoDbContext(DbContextOptions<DemoDbContext> options)
+        : base(options)
+    {
+    }
+}
